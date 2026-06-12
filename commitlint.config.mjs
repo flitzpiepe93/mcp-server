1c44f7a9b14a50e48174680ef410ef5bcc680f1b
ba33812 <marcel.kennert@gmx.de>
2026-06-12 18:21:49 +0200

    chore(setup): add pre-commit hooks and commit message linting

---
+++ commitlint.config.mjs
@@ -0,0 +1,4 @@
+// required by precommit to enforce conventional commits, see: https://commitlint.js.org/guides/getting-started
+export default {
+  extends: ["@commitlint/config-conventional"],
+};
